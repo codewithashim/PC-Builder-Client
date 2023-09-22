692046bbfbafe225b0bda8f3ecf66fd7be342799
Ashim Rudra Paul <81712334+codewithashim@users.noreply.github.com>
2023-09-22 22:51:55 +0600

    api is live

--- src/Utils/network.js
+++ src/Utils/network.js
@@ -1,2 +1,2 @@
-export const ApiBasdUrl = "http://localhost:8000/api/v1";
-// export const ApiBasdUrl = "https://pc-builder-server-indol.vercel.app/api/v1";
+// export const ApiBasdUrl = "http://localhost:8000/api/v1";
+export const ApiBasdUrl = "https://pc-builder-server-five.vercel.app/api/v1";
